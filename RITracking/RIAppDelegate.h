0e49fca294ac24c293877fbf1777a12a469aa991
Martin Biermann <info@martinbiermann.com>
2014-03-10 09:31:36 +0100

    Initial Commit

---
+++ RITracking/RIAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  RIAppDelegate.h
+//  RITracking
+//
+//  Created by Martin Biermann on 10/03/14.
+//  Copyright (c) 2014 Martin Biermann. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RIAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ RITracking/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  RITracking
+//
+//  Created by Martin Biermann on 10/03/14.
+//  Copyright (c) 2014 Martin Biermann. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "RIAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([RIAppDelegate class]));
+    }
+}
